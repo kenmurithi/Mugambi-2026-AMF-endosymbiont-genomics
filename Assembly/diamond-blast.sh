9f0008eadc5de33fb0e57f7a16a49d77cbfb399a
Ken Murithi <59683723+kenmurithi@users.noreply.github.com>
2026-01-07 11:29:26 -0500

    Update diamond-blast.sh

--- Assembly/diamond-blast.sh
+++ Assembly/diamond-blast.sh
@@ -1,1 +1,2 @@
 
+/shared/programs/diamond-0.9.14/diamond blastx -d /shared/programs/nr_diamond/nr_diamond_out -o gigaspora_yahs-hifiasm-candi_blastx.out -q scaffolds.fa -f 0 --max-hsps 1 --top 10 -e 1e-5 -p 40
