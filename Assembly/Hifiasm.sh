f421cd8f6d9855abfe0378f4119cd8ae26d8a833
Ken Murithi <59683723+kenmurithi@users.noreply.github.com>
2026-01-07 07:19:02 -0500

    Update Hifiasm.sh

--- Assembly/Hifiasm.sh
+++ Assembly/Hifiasm.sh
@@ -1,9 +1,19 @@
-```bash
-hifiasm -o gigaspora_hifiasm.asm -t25 --n-hap 1 --hg-size 773m \
-        --h1 ../gigaspora_reads/gigaspora_hic/results/gmarg-bmin_1274592/HiC/gmarg-bmin_1274593_S3HiC_R1.fastq.gz \
-        --h2 ../gigaspora_reads/gigaspora_hic/results/gmarg-bmin_1274592/Hi-C/gmarg-bmin_1274593_S3HiC_R2.fastq.gz \
-        ../gigaspora_reads/gigaspora_hifi.fastq.gz
-        
-awk '$1 == "S" {print ">"$2"\n"$3}' gigaspora_hifiasm.asm.hic.p_ctg.gfa > gigaspora_p_ctg.fasta
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Run hifiasm with Hi-C and HiFi reads
+hifiasm \
+  -o gigaspora_hifiasm.asm \
+  -t 25 \
+  --n-hap 1 \
+  --hg-size 773m \
+  --h1 ../gigaspora_reads/gigaspora_hic/results/gmarg-bmin_1274592/HiC/gmarg-bmin_1274593_S3HiC_R1.fastq.gz \
+  --h2 ../gigaspora_reads/gigaspora_hic/results/gmarg-bmin_1274592/Hi-C/gmarg-bmin_1274593_S3HiC_R2.fastq.gz \
+  ../gigaspora_reads/gigaspora_hifi.fastq.gz
+
+# Convert primary contigs from GFA to FASTA
+awk '$1=="S"{print ">"$2"\n"$3}' gigaspora_hifiasm.asm.hic.p_ctg.gfa > gigaspora_p_ctg.fasta
+
+# Rename output
 mv gigaspora_p_ctg.fasta Gi_marg_hifiasm_contigs.fa
-```
+
