de3f8c20de48c33dd8f8cdadb67c6908d63a387f
Ken Murithi <59683723+kenmurithi@users.noreply.github.com>
2026-01-07 11:34:40 -0500

    Update remove-contaminants.sh

--- Assembly/remove-contaminants.sh
+++ Assembly/remove-contaminants.sh
@@ -1,8 +1,8 @@
 
-
-
-
-
-
+# Step 1: Manually inspect BLAST hits (from )
 grep -A 5 "Query=" gigaspora_canu_blastx.out | head
+
+# Step 2: Remove contaminant contigs
+### The file contaminants2remove.txt contains contig IDs
+### identified as non-AMF based on BLASTX results 
 seqkit grep -v -f ./BlastOut/conataminants2remove.txt ./Hifiasm/Gi_marg_hifiasm_contigs.fa > Gi_marg_hifiasm_cleaned_contigs.fa
