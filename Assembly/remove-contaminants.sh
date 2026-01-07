8b947e513e6134bc35247fb0c40d0d0c843a3082
Ken Murithi <59683723+kenmurithi@users.noreply.github.com>
2026-01-07 07:28:28 -0500

    Update remove-contaminants.sh

--- Assembly/remove-contaminants.sh
+++ Assembly/remove-contaminants.sh
@@ -1,1 +1,8 @@
 
+
+
+
+
+
+grep -A 5 "Query=" gigaspora_canu_blastx.out | head
+seqkit grep -v -f ./BlastOut/conataminants2remove.txt ./Hifiasm/Gi_marg_hifiasm_contigs.fa > Gi_marg_hifiasm_cleaned_contigs.fa
