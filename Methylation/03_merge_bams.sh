2051472fd34cd2793952600d85cda3ace19fde6a
Ken Murithi <59683723+kenmurithi@users.noreply.github.com>
2026-01-08 11:14:46 -0500

    Update 03_merge_bams.sh

--- Methylation/03_merge_bams.sh
+++ Methylation/03_merge_bams.sh
@@ -1,1 +1,9 @@
+# Merge aligned BAM files
 
+THREADS=30
+
+samtools merge \
+  -@ ${THREADS} \
+  gigaspora_aligned_merged_bc2025_bc2026.bam \
+  gigaspora_aligned_bc2025.bam \
+  gigaspora_aligned_bc2026.bam
