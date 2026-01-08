f81389258c294f3c95e08537e7f2dfed9e1dfda4
Ken Murithi <59683723+kenmurithi@users.noreply.github.com>
2026-01-08 11:15:44 -0500

    Update 04_call_cpg.sh

--- Methylation/04_call_cpg.sh
+++ Methylation/04_call_cpg.sh
@@ -1,1 +1,15 @@
+BAM=gigaspora_aligned_merged_bc2025_bc2026.bam
+PREFIX=Gi_margarita
+THREADS=30
 
+pb-CpG-tools-v2.3.2-x86_64-unknown-linux-gnu/bin/aligned_bam_to_cpg_scores \
+  --bam ${BAM} \
+  --output-prefix ${PREFIX} \
+  --model pb-CpG-tools-v2.3.2-x86_64-unknown-linux-gnu/models/pileup_calling_model.v1.tflite \
+  --threads ${THREADS} \
+  --min-mapq 0 \
+  --min-coverage 10
+
+# Keep methylated CpG sites only
+awk '$9 > 0.0' ${PREFIX}-total-CpG.bed \
+  > ${PREFIX}_total-met-CpG.bed
