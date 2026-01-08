69c8dea96c515e3264c36f2e37bc6737704c47f1
Ken Murithi <59683723+kenmurithi@users.noreply.github.com>
2026-01-08 11:35:25 -0500

    Update 07_gene_density_vs_compartments.sh

--- Methylation/07_gene_density_vs_compartments.sh
+++ Methylation/07_gene_density_vs_compartments.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
+
+# Gene density per Hi-C compartment (50 kb bins)
+
+BINS=Gi_marg_50kb.bed
+GENES=Gi_marg_genes.bed
+
+# ---- Count genes per bin ----
+bedtools intersect \
+  -a ${BINS} \
+  -b ${GENES} \
+  -c \
+> Gi_marg_50kbgenes.bed
+
+# ---- bmin compartments ----
+bedtools intersect \
+  -a Gi_marg_50kbgenes.bed \
+  -b Gi_margarita-bmin-all-compartments.bed \
+  -wa -wb | \
+awk '{print $1,$2,$3,$4,$8}' OFS="\t" \
+> Gi_marg_bmincomp_50kbgenes.bed
+
+awk '{sum[$5]+=$4; count[$5]++} END {for (c in sum) print c, sum[c]/count[c]}' \
+Gi_marg_bmincomp_50kbgenes.bed
 
