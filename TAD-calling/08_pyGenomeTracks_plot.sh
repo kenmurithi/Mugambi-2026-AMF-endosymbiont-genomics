72be31a41944fee3adb3676e7037bd59e7232a08
Ken Murithi <59683723+kenmurithi@users.noreply.github.com>
2026-01-08 12:19:53 -0500

    Create 08_pyGenomeTracks_plot.sh

---
+++ TAD-calling/08_pyGenomeTracks_plot.sh
@@ -0,0 +1,7 @@
+# Plot Hi-C matrix, TADs, and scores for a genomic region
+
+pyGenomeTracks \
+  --tracks tracks.ini \
+  --region scaffold_3:1-25597707 \
+  -o scaffold_3-TADS-pygenome.png \
+  --dpi 600
