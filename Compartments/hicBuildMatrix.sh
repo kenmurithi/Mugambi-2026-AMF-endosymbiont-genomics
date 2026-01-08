fe3223cc45838b17301539b758fdfe92e3033659
Ken Murithi <59683723+kenmurithi@users.noreply.github.com>
2026-01-08 10:44:41 -0500

    Update hicBuildMatrix.sh

--- Compartments/hicBuildMatrix.sh
+++ Compartments/hicBuildMatrix.sh
@@ -1,1 +1,10 @@
-
+hicBuildMatrix \
+  --samFiles R1.sam R2.sam \
+  --outFileName Gi-margarita.h5 \
+  --QCfolder qc_reports \
+  --binSize 10000 15000 20000 25000 30000 40000 \
+  --minMappingQuality 10 \
+  --restrictionCutFile combined_rest_sites.bed \
+  --restrictionSequence GATC GAATC GAGTC GATTC GACTC CTAAG CTGAG CTTAG CTCAG TTAA \
+  --danglingSequence GATC AAT AGT ATT ACT TAA TGA TTA TCA TA \
+  --threads 30
