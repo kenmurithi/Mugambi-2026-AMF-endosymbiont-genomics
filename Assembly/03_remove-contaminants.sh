8826cf597832a182f4f1ecb00e35d104f12517f4
Ken Murithi <59683723+kenmurithi@users.noreply.github.com>
2026-02-23 14:00:18 -0500

    Update 03_remove-contaminants.sh

--- Assembly/03_remove-contaminants.sh
+++ Assembly/03_remove-contaminants.sh
@@ -1,6 +1,6 @@
 
 # Step 1: Manually inspect BLAST hits (from diamond-blast.sh)
-grep -A 5 "Query=" gigaspora_canu_blastx.out | head
+grep -A 5 "Query="  Gi_marg_hifiasm_contigs.fa_blastx.out | head
 
 # Step 2: Remove contaminant contigs
 ### The file contaminants2remove.txt contains contig IDs
