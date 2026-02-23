796f89520b09dbe372dea55d5cc5ccf924a3be97
Ken Murithi <59683723+kenmurithi@users.noreply.github.com>
2026-02-23 14:03:58 -0500

    Update 05_scaffolding.sh

--- Assembly/05_scaffolding.sh
+++ Assembly/05_scaffolding.sh
@@ -2,4 +2,4 @@
 # Step 1: Run *arima-pipeline.sh* to obtain the BED file needed for YAHS.
 
 #s Step 2: Scaffolding
-yahs -e GATC,GANTC,CTNAG,TTAA  Gi_marg_hifiasm_cleaned_contigs.fa ./deduplicated/files/gigaspora_Canu_bmin_HiC_rep1.bed
+yahs -e GATC,GANTC,CTNAG,TTAA  Gi_marg_hifiasm_cleaned_contigs.fa ./deduplicated/files/gigaspora_HiC_rep1.bed
