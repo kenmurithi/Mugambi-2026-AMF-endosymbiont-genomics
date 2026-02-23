723f6a643adfdb608136ab83b97e2b2e7d006927
Ken Murithi <59683723+kenmurithi@users.noreply.github.com>
2026-02-23 14:07:35 -0500

    Update secretome.sh

--- Annotation/secretome.sh
+++ Annotation/secretome.sh
@@ -1,1 +1,13 @@
+# Secreted proteins and effectors
 
+signalp6 --fastafile Gigaspora_margarita_BEG.proteins.fa --organism eukarya --output_dir SignalP-EffectorP --format txt --mode fast
+
+awk '{print $1}' SignalP-EffectorP/region_output.gff3 | grep -v '#' | sort | uniq > 02-SP-list.txt
+awk 'FNR==NR{a[$0];next} /^>/{val=$0;sub(/^>/,"",val);flag=val in a?1:0} flag' 02-SP-list.txt Giga-prot-simple.fa > 03-SP-sequences.fa
+
+python /home/eniac/jordana/EffectorP-3.0/EffectorP.py -i 03-SP-sequences.fa -o 04-Gigaspora-mar-effector-pred.txt
+
+grep -v 'Non-effector' 04-Gigaspora-mar-effector-pred.txt | grep -v '#' | awk '{print $1}' > 05-Effector-list.txt
+
+awk 'FNR==NR{a[$0];next} /^>/{val=$0;sub(/^>/,"",val);flag=val in a?1:0} flag' 05-Effector-list.txt Giga-prot-
+simple.fa > 06-Giga-effectors-sequences.fa
