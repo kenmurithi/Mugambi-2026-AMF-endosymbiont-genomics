c314c3c321b8f2bd4ccbff62448103b114e95e82
Ken Murithi <59683723+kenmurithi@users.noreply.github.com>
2026-02-23 13:59:25 -0500

    Update 02_diamond-blast.sh

--- Assembly/02_diamond-blast.sh
+++ Assembly/02_diamond-blast.sh
@@ -1,2 +1,2 @@
 
-/shared/programs/diamond-0.9.14/diamond blastx -d /shared/programs/nr_diamond/nr_diamond_out -o gigaspora_yahs-hifiasm-candi_blastx.out -q scaffolds.fa -f 0 --max-hsps 1 --top 10 -e 1e-5 -p 40
+/shared/programs/diamond-0.9.14/diamond blastx -d /shared/programs/nr_diamond/nr_diamond_out -o Gi_marg_hifiasm_contigs.fa_blastx.out -q Gi_marg_hifiasm_contigs.fa -f 0 --max-hsps 1 --top 10 -e 1e-5 -p 40
